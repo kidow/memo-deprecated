88c51988e0ada131f5b9266e9fee9478e20dfe21
kidow <wcgo2ling@gmail.com>
2022-04-14 23:01:45 +0900

    typescript

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="container mx-auto">App</div>
-}
-
-export default App
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react'
+
+export interface Props {}
+interface State {}
+
+const App: FC<Props> = () => {
+  return <>App</>
+}
+
+export default App
