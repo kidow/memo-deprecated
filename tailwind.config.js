eef37f7b9c064ae61552f25bcc693f22a908824e
kidow <wcgo2ling@gmail.com>
2022-06-13 22:09:35 +0900

    install tailwindcss latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
